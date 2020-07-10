aef2409a22e2b47c649ea32cc9cbce763fd430b9
yosefsklar <yosef.sklar@gmail.com>
2020-07-10 00:21:50 -0400

    Updates

---
+++ precache-manifest.87ec1a59b0dd9f53588606e19aea3459.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d36640216688a9b46b9b24a8a0e617",
+    "url": "/bashamayim-hi/index.html"
+  },
+  {
+    "revision": "9bd0ef1f2a671ae0f448",
+    "url": "/bashamayim-hi/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "74bd38b22e24ae2daff3",
+    "url": "/bashamayim-hi/static/js/2.4a4347cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bashamayim-hi/static/js/2.4a4347cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd0ef1f2a671ae0f448",
+    "url": "/bashamayim-hi/static/js/main.90773ad3.chunk.js"
+  },
+  {
+    "revision": "b2aacfab54104160f515",
+    "url": "/bashamayim-hi/static/js/runtime-main.bbac2c5d.js"
+  },
+  {
+    "revision": "0135d99ee2b8bbdde06c296ba8bf6afe",
+    "url": "/bashamayim-hi/static/media/SRHirsch.0135d99e.png"
+  },
+  {
+    "revision": "f597f27654f21837524e2d1d16c87653",
+    "url": "/bashamayim-hi/static/media/cloudbackgroundRed2.f597f276.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bashamayim-hi/static/media/logo.5d5d9eef.svg"
+  }
+]);

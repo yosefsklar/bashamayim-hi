a8c1b9147457666fdcbaa7d22445763f1c0a438a
yosefsklar <yosef.sklar@gmail.com>
2020-07-19 23:16:41 -0400

    ///

--- src/Containers/BHGame.js
+++ src/Containers/BHGame.js
@@ -54,7 +54,7 @@ export default class BHGame extends Component {
         this.setState({
             textUrlName : this.state.parsha.textUrlName,
             startChapter : this.state.parsha.startChapter
-        },() => this.props.history.push(`${this.props.match.url}gamePlay`))
+        },() => this.props.history.push(`${this.props.match.url}/gamePlay`))
     }
 
 
@@ -70,7 +70,7 @@ export default class BHGame extends Component {
             endVerse: 0,
             level:'',
             gameNumber: this.state.gameNumber + 1
-        }, () => this.props.history.push(`${this.props.match.url}gameDefault`))
+        }, () => this.props.history.push(`${this.props.match.url}/gameDefault`))
     }
 
 
@@ -83,7 +83,7 @@ export default class BHGame extends Component {
         return (
             <div>
                 <Switch>
-                    <Route path={`${this.props.match.url}gamePlay`}>
+                    <Route path={`${this.props.match.url}/gamePlay`}>
                         <BHRound
                             level = {"hard"}
                             newGame = {this.setGameDefault}
@@ -92,7 +92,7 @@ export default class BHGame extends Component {
                             startChapter={this.state.parsha.startChapter}
                         />
                     </Route>
-                    <Route path={`${this.props.match.url}gameDefault`}>
+                    <Route path={`${this.props.match.url}/gameDefault`}>
                         <GameDefault setParsha={this.setParsha}
                                      parsha={this.state.parsha}
                                      textUrlName={this.state.textUrlName}

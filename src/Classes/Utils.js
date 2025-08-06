cf4938b5edbf375973c0f6cdcd9b45f87e3dc513
yosefsklar <yosef.sklar@gmail.com>
2025-08-06 08:19:31 -0400

    add docstrings to Utils class

--- src/Classes/Utils.js
+++ src/Classes/Utils.js
@@ -1,18 +1,31 @@
+/**
+ * Utility class providing various text manipulation methods.
+ */
 export default class Utils {
 
+    /**
+     * Removes all HTML tags, including anchor tags, from each string in the provided array.
+     * @param {string[]} textArr - Array of strings potentially containing HTML.
+     * @returns {string[]} Array of strings with HTML tags removed.
+     */
     removeHTML(textArr) {
         return textArr.map(x => x.replace(/<\s*a[^>]*>(.*?)<\s*\/\s*a>/g, "").replace(/<[^>]*>/g, ""));
-
     }
 
-    /*
-    * Takes in string returns string
-    * */
-
+    /**
+     * Removes Hebrew vowel marks (niqqud) from the input string.
+     * @param {string} rawString - String containing Hebrew text with possible vowel marks.
+     * @returns {string} String with vowel marks removed.
+     */
     stripVowels(rawString) {
-        return rawString.replace(/[\u0591-\u05C7]/g, "")
+        return rawString.replace(/[\u0591-\u05C7]/g, "");
     }
 
+    /**
+     * Randomly shuffles the elements of the given array.
+     * @param {any[]} array - Array to be shuffled.
+     * @returns {void}
+     */
     shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -20,14 +33,13 @@ export default class Utils {
         }
     }
 
-    /*
-    * Takes in string returns array of words
-    * */
-
-    stripCantillation = (words) => {
+    /**
+     * Removes Hebrew cantillation marks, punctuation, and bracketed text from the input string,
+     * then splits the cleaned string into an array of words.
+     * @param {string} words - String containing Hebrew text with cantillation marks and punctuation.
+     * @returns {string[]} Array of words with cantillation marks and punctuation removed.
+     */
+    stripCantillation(words) {
         return words.replace(/[\u0591-\u05AF\u05c0]|\(פ\)|\(ס\)|\[(.*?)]/g, "").split(/[\s\u05BE]+/);
-
-    };
-
-
+    }
 };

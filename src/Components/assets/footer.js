9d67dbeb4b79fbd59c4260482b0144d855da4adf
yosefsklar <yosef.sklar@gmail.com>
2025-08-13 21:20:12 -0400

    footer

--- src/Components/assets/footer.js
+++ src/Components/assets/footer.js
@@ -11,7 +11,7 @@ export class Footer extends Component {
                 <p>Powered by
                     <a href="https://github.com/Sefaria/Sefaria-Project/wiki/API-Documentation"> Sefaria APIs</a>
                 </p>
-                <p>Free background photos from
+                <p className={classes.lastFooter}>Free background photos from
                     <a href="https://pngtree.com/free-backgrounds"> pngtree.com</a>
 
                 </p>
